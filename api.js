a54f027293610aac3be2d9d5e459ef7fc6ffbb4b
yomark14 <39974565+yomark14@users.noreply.github.com>
2022-05-19 16:02:55 -0500

    "apijs"

--- api.js
+++ api.js
@@ -1,31 +1,33 @@
-// esta funcion es porque no habia botones y cada vez que se refrescaba la pagina
-// se ejecutaba una funcion para generar un numero random
-//     document.addEventListener('DOMContentLoaded', () => {
-//         const random = getRandom(1, 151) 
-//         fetchData(random)
-//     })
-//  const getRandom = (min, max) => {
-//     return Math.floor(Math.random() * (max - min)) + min;
-//  }
-
-// el numero random se va como parametro a este funcion como 'id' y en la liga la
-// la incrustaba para que fuera cambiando de poquemon por el id de la api
-
-const URL = "https://pokeapi.co/api/v2/pokemon/";
-const fetchData = async (id)=> {
-    try {
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-        const data = await res.json()
-        mostrarCarta(data)
-    }catch (error) {
-        console.log(error);
-    }
+document.addEventListener('DOMContentLoaded', () => {
+    const random = getRandom(1, 151) 
+    fetchData(random)
+})
+const getRandom = (min, max) => {
+return Math.floor(Math.random() * (max - min)) + min;
 }
 
-//esta parte es donde se va a cambiar la imagen, lo que no tengo claro es que me voy a traer del html porque ya esta revuelto con el bootstrap y no esta muy claro para mi
+
+
+const fetchData = async (id)=> {
+try {
+const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+const data = await res.json()
+mostrarCarta(data)
+}catch (error) {
+console.log(error);
+}
+}
+
+
 
 const mostrarCarta = (pokemon) => {//poquemon es la data de la funcion anterior
-    console.log(pokemon)
-    const template = document.getElementById('ImagenPokemon')// aqui tendria que traerme como el contenedor que contiene la imagen
-    const imagen = document.getElementById('ImagenPokemon').setAttribute// aqui tendria que traerme el tag de la imagen y meterle el atributo de src dinamicamente.
+console.log(pokemon)
+const imagen = document.querySelector('#ImagenPokemon')
+document.querySelector('#aquiVaElPoke').setAttribute('src', pokemon.sprites.other.dream_world.front_default)
+
+imagen.appendChild(template)
+
+
+
+
 }
