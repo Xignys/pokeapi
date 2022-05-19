5ea300ffb4f79e50a29f25971fcd231b28b5217b
Arnoldo Martinez <89050900+FrancoMtz-ctrl@users.noreply.github.com>
2022-05-19 16:44:02 -0500

    Add files via upload
    los ultimos ultimos

--- app.js
+++ app.js
@@ -24,9 +24,19 @@
 const mostrarCarta = (pokemon) => {//poquemon es la data de la funcion anterior
     console.log(pokemon)
     const imagen = document.querySelector('#ImagenPokemon')
+    const neimo = document.querySelector('#NombrePokemon').innerHTML
+    const id = document.querySelector('#CajaNumPoke').innerHTML
     document.querySelector('#aquiVaElPoke').setAttribute('src', pokemon.sprites.other.dream_world.front_default)
+    
+    document.querySelector('#nombrePoke').innerHTML= pokemon.name
+    document.querySelector('#numPoke').innerHTML= pokemon.id
 
-    imagen.appendChild(template)
+
+
+
+
+    imagen.appendChild(imagen)
+    neimo.appendChild(neimo)
 
 
     
